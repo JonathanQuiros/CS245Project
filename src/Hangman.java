42ae533fcdaa226968e0179d5ae7fec8f87f0ea6
Jonathan Quiros Perez <jonathanquiros@b01-aruba1-guest-10-110-65-108.wlan.cpp.edu>
2017-01-18 16:51:26 -0800

    First commit

---
+++ src/Hangman.java
@@ -0,0 +1,37 @@
+/**
+ *
+ * @author Bad and Boujee
+ */
+
+import java.awt.event.*;
+import javax.swing.*;
+import my.HangmanUI.*;
+
+public class Hangman extends JFrame {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws Exception {
+        IntroScreen intro = new IntroScreen();
+        MainScreen mainScreen = new MainScreen();
+        
+        intro.setVisible(true);
+        
+        int transitionDelay = 3000;
+        
+        ActionListener taskPerformer = new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                intro.setVisible(false);
+                mainScreen.setVisible(true);
+            }
+        };
+              
+        Timer timer = new Timer(transitionDelay, taskPerformer);
+        timer.setRepeats(false);
+        timer.start();
+        
+        Thread.sleep(5000);
+    }
+}
+    
